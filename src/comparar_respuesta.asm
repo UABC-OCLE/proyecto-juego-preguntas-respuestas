14a42b361bd32eae0ae1e2df0947aa01a91532ca
ValentinR37 <158795066+ValentinR37@users.noreply.github.com>
2024-05-18 01:05:03 +0000

    Prueba: Sin exito

--- src/comparar_respuesta.asm
+++ src/comparar_respuesta.asm
@@ -13,60 +13,69 @@
 
 
 section .text ;obligatorio iniciar con esto mismo
-
-global comprobante_resultados
+    global comprobante_resultados
 
 
 comprobante_resultados:
     xor rax, rax
-    mov rcx, 0 ;este mismo contador indicara el final de los elementos de la tabla.
+    xor rcx, rcx ;este mismo contador indicara el final de los elementos de la tabla.
 
 verificación:
+    cmp rcx, 5 ;aqui es donde verificamos si la cadena ha terminado
+    je .termino ;saltamos a la parte final 
     ;aqui es donde compararemos tanto el array de preguntas y el de respuestas, primero inicializando el contador, se va a inicializar en 0 para ir sumando de forma adecuada.
     mov al, [rdi + rcx] ;almacena respuesta de usuario rdi[rcx] (como si fuese rdi[i o 0 pues])
     cmp al, [rsi + rcx] ;ahora la comparar con el elemento rsi[rcx] de las respuestas correctas
-    je incremento ;iremos a la linea donde se incrementa el rax
+    je .incremento ;iremos a la linea donde se incrementa el rax
     inc rcx
-    cmp rcx, 5 ;aqui es donde verificamos si la cadena ha terminado
-    je termino ;saltamos a la parte final 
     jmp verificación ;saltamos de nueva cuenta al verificador para poder seguir iterando
 
-incremento:
+.incremento:
     inc rax ;aqui aumentamos en 1 el rax
-    inc rcx ;el rcx se aumenta pues porque ocupamos que aumente realmente ya que si no pues nos quedamos que si respondes de forma incorrecta solo ahi avanzara
     cmp rcx, 5 ;comparamos si ya termino rcx con todo el arreglo
-    je termino
+    je .termino
+    inc rcx ;el rcx se aumenta pues porque ocupamos que aumente realmente ya que si no pues nos quedamos que si respondes de forma incorrecta solo ahi avanzara
     jmp verificación
 
 
-termino:
+.termino:
     cmp rax, 0
-    je cero
+    je .resultados_cero
     cmp rax, 3
-    jae Aprobado
+    jae .resultados_aprobado
     cmp rax, 2
-    jbe Reprobado
+    jbe .resultados_reprobado
 
-;Aquí es donde se envian los resultados
-Aprobado:
-    mov rdi, msg1
-    jmp resultados
+;Aquí es donde se envian los resultados e imprimen
 
-Reprobado:
-    mov rdi, msg2
-    jmp resultados
+.resultados_aprobado:
+    mov rsi, msg1
+    mov rdx, msg1_len
+    jmp .resultados_total
 
-cero:
-    mov rdi, msg3
-    jmp resultados
+.resultados_reprobado:
+    mov rsi, msg2
+    mov rdx, msg2_len
+    jmp .resultados_total
 
-resultados:
-    mov rdi, 0
+.resultados_cero:
+    mov rsi, msg3
+    mov rdx, msg3_len
+    jmp .resultados_total
+
+.resultados_total:
+    mov rax, 1
+    mov rdi, 1
     syscall
     ret
 
 
 section .rodata
-    msg1: db 'Aprobado. Aciertos = ', 0
-    msg2: db 'Reprobado. Aciertos = ', 0
-    msg3: db 'Wow. Creo que toca estudiar un poco, tuviste cero aciertos...', 0
+    msg1: db "Aprobado, Felicidades!, tuviste un total de aciertos de: ", 10
+    msg1_len equ $ - msg1
+
+    msg2: db "Reprobado, no pasa nada!, tuviste un total de aciertos de: ", 10
+    msg2_len equ $ - msg2
+
+    msg3: db "Wow... Creo que toca estudiar un poco, tuviste cero aciertos...", 10
+    msg3_len equ $ - msg3
--- src/main.c
+++ src/main.c
@@ -10,7 +10,6 @@ Por ende, voy hacerla de capitales de America*/
 extern void comprobante_resultados(char Usuelec[], char CorrectRes[]);
 
 int main(){
-    char usuRes; //queremos pedirle al usuario una respuesta que se almacenara aqui
     char CorrectRes[5] = {'a', 'b', 'a', 'd', 'c'}; //las respuestas correctas aqui yaceran, porque si no tendria que estarse utilizando mucho if y else que complicarian el codigo
     char Usuelec[5]; //para las respuestas que el usuario anote, tomando inspiracion del ejemplo puesto por el profe me parecio buena idea agregarlo
     //sin embargo, lo que yo voy a hacer es con el usuRes el recibir las respuestas que se almacenaran en Usuelec.
@@ -18,37 +17,35 @@ int main(){
     printf("\n¿Cuál es la capital de México?\n");
     printf("\na) Ciudad de México   b) Baja California   c) Guadalajara   d) Acapulco\n");
     printf("\nRespuesta: ");
-    scanf(" %c", &usuRes);
-    Usuelec[0] = usuRes;  //Almacenamos la respuesta generada por el usuario dentro de la lista donde entraran todas las respuestas.
-    //esto debido a que luego sera comparada con las respuestas correctas
+    scanf(" %c", &Usuelec[0]);
 
     printf("\n¿Cuál es la capital de Estados Unidos?\n");
     printf("\na) Texas  b) Washington D.C   c) California   d) Florida\n");
     printf("\nRespuesta: ");
-    scanf(" %c", &usuRes);
-    Usuelec[1] = usuRes;
+    scanf(" %c", &Usuelec[1]);
 
     printf("\n¿Cuál es la capital de Argentina?\n");
     printf("\na) Buenos Aires   b) Rosario   c) Córdoba   d) Catamarca\n");
     printf("\nRespuesta: ");
-    scanf(" %c", &usuRes);
-    Usuelec[2] = usuRes;
+    scanf(" %c", &Usuelec[2]);
 
     printf("\n¿Cuál es la capital de Brazil?\n");
     printf("\na) Paraná   b) Sao Paulo   c) Rio de Janeiro   d) Brasilia\n");
     printf("\nRespuesta: ");
-    scanf(" %c", &usuRes);
-    Usuelec[3] = usuRes;
+    scanf(" %c", &Usuelec[3]);
 
     printf("\n¿Cuál es la capital de Canadá?\n");
     printf("\na) Nueva Escocia   b) Ontario   c) Ottawa   d) Quebec\n");
     printf("\nRespuesta: ");
-    scanf(" %c", &usuRes);
-    Usuelec[4] = usuRes;
+    scanf(" %c", &Usuelec[4]);
 
     //una vez hechas las preguntas, lo que ahora tenemos que hacer es irnos a ensamblador para las respuestas y mensajes.
 
+    printf("\n\n");
+
     comprobante_resultados(Usuelec, CorrectRes);
+
+    printf("\n\n");
 
     return 0;
 }
